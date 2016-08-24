fc1222b77efcc2fc09f76127ef90cde5cbdd3402
psantra <pijushsantra29@gmail.com>
2016-08-23 19:59:49 -0500

    first commit

---
+++ app/src/main/java/com/cognizant/app/beacondetectorapp/SecondPageActivity.java
@@ -0,0 +1,13 @@
+package com.cognizant.app.beacondetectorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+    }
+}
